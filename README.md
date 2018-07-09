a8de4ae531a21f2b79fe24a32a5925146b30ce46
t1agob <tiagob@microsoft.com>
2018-07-09 19:23:35 +0100

    first commit
    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# DevSpacesSample
-Azure Dev Spaces Demo - Prepared for Azure Wednesdays Portugal
+
+# Azure DevSpaces Sample
+
+Azure Dev Spaces Sample - Prepared for [Azure Wednesdays Portugal](https://www.meetup.com/Azure-Wednesdays-Portugal/) meetup.
+
+## Chuck Norris Facts
+
+This sample is composed of a web frontend and an API that generates nerdy 'Chuck Norris' quotes. Each service is a different container that can be deployed and debugged seperately.
+
+## Objective
+
+The objective of this sample is to show how to use Azure DevSpaces in AKS and see how this will greatly improve the developer experience.
---
+++ chuck.web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using chuck.web.Models;
+using System.Net.Http;
+
+namespace chuck.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        static HttpClient client = new HttpClient();
+
+        public IActionResult Index()
+        {
+            ViewBag.Fact = "Just ask Chuck!";
+
+            return View();
+        }
+
+        public async Task<IActionResult> RandomFact()
+        {
+            HttpResponseMessage response = await client.GetAsync("http://chuckapi/api/quotes");
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.Fact = await response.Content.ReadAsStringAsync();
+                //product = await response.Content.ReadAsAsync<Product>();
+            }
+
+            
+
+            return View("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
